f6f527b1b963fb124140c65bff9389e736874051
accesable <trannhutanh654@gmail.com>
2023-01-11 20:08:38 +0700

    first init

---
+++ app.js
@@ -0,0 +1,9 @@
+//Load express module
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000, ()=> console.log('Example app listenning at http://localhost:3000'))
